85e1c24c37bc830453e6c269ae59880b106fe9ae
Eugene Petrenko <Eugene.Petrenko@jetbrains.com>
2016-05-09 17:48:30 +0200

    generate request

--- test.sh
+++ test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# git upload-pack . <$(echo 0000) >heads
+
+echo >> wants
+while read i; do
+  l="0032want $(cut -c 5-40 - <<<$i)"
+  echo $l >> wants  
+  echo $l
+done < heads
 
 
-cat request | git upload-pack . 
+# cat request | git upload-pack . 
